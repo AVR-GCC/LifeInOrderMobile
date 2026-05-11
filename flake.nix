f0f578aa8f9f2bbca25ca6ddf9f7f92e39a7bf28
AVR-GCC <ogoun.d@gmail.com>
2026-05-11 21:39:30 +0300

    build for android

--- flake.nix
+++ flake.nix
@@ -1,20 +1,29 @@
 # flake.nix
 {
-  nixpkgs.config.allowUnfree = true;
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        };
+      };
       androidSdk = (pkgs.androidenv.composeAndroidPackages {
-        platformVersions = [ "34" ];
-        buildToolsVersions = [ "34.0.0" ];
+        platformVersions = [ "36" ];
+        buildToolsVersions = [ "35.0.0" "36.0.0" ];
         includeNDK = true;
+        ndkVersions = [ "27.1.12297006" ];
+        cmakeVersions = [ "3.22.1" ];
       }).androidsdk;
     in pkgs.mkShell {
-      buildInputs = [ androidSdk pkgs.jdk17 pkgs.nodejs ];
+      buildInputs = [ androidSdk pkgs.jdk17 pkgs.nodejs pkgs.cmake ];
       ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+      ANDROID_NDK_HOME = "${androidSdk}/libexec/android-sdk/ndk/27.1.12297006";
       JAVA_HOME = "${pkgs.jdk17}";
     };
   };
