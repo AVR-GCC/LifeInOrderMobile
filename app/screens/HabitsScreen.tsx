cd78a9e829ad90c6275c773821accbc3b7f2488f
AVR-GCC <ogoun.d@gmail.com>
2026-08-27 13:56:16 +0300

    Habitseses

--- app/screens/HabitsScreen.tsx
+++ app/screens/HabitsScreen.tsx
@@ -41,7 +41,7 @@ export const HabitsScreen: React.FC<HabitsScreenProps> = ({ data, switchHabits, 
         >
           <BackArrow />
         </TouchableOpacity>
-        <Text style={styles.title}>Habitses</Text>
+        <Text style={styles.title}>Habitseses</Text>
       </TitleBar>
       <View style={styles.dayContainer}>
         <ScrollView style={styles.scrollContainer}>
