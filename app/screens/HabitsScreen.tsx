271cb96ce0c19c4858c49f83949c351930448fc4
AVR-GCC <ogoun.d@gmail.com>
2026-08-28 19:40:15 +0300

    Manually set version

--- app/screens/HabitsScreen.tsx
+++ app/screens/HabitsScreen.tsx
@@ -41,7 +41,7 @@ export const HabitsScreen: React.FC<HabitsScreenProps> = ({ data, switchHabits, 
         >
           <BackArrow />
         </TouchableOpacity>
-        <Text style={styles.title}>Habitatasss</Text>
+        <Text style={styles.title}>Habitatssas</Text>
       </TitleBar>
       <View style={styles.dayContainer}>
         <ScrollView style={styles.scrollContainer}>
